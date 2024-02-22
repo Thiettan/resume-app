db97aaf82ae9267e363227d8b3d73d09e0ae1102
Tan Bui <ttb39@scarletmail.rutgers.edu>
2024-02-22 15:48:32 -0800

    skeleton built with bootstrap integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { ResumeData } from './data';
+import { useState } from 'react';
+
+function ContentBody({data}){
+  console.log(data)
+  return(
+    <div className="content-body">
+      <h4>{data.position} @{data.company}</h4>
+      <p>{data.content}</p>
+    </div>
+  )
+}
 
 function App() {
+
+  const [index,setIndex] = useState(0)
+
+  function handleClick(e){
+  setIndex(e.target.tabIndex)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="resume-app container">
+      <header className="app-header">
+        <h3>My Experience</h3>
       </header>
+      <div className="row">
+        <div className="col-4">
+        <nav className='nav-list' role="tablist">
+          {ResumeData.map((item,i)=>(
+            <button
+              className={i == index ? 'active' : ''}
+              tabIndex={i} key={item.company}
+              onClick={handleClick}>{item.company}
+            </button>
+          ))}
+          </nav>
+        </div>
+            <div className="col-8"> <ContentBody data={ResumeData[index]} /></div>
+       
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const ResumeData = [{
+        company: "Freelance",
+        position: "Web Consultant",
+        year: "2018 - Present",
+        content: "Build, style, and ship high-quality websites, design systems, mobile apps, and digital experiences for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and spearheading the development of internal tools."
+    },
+    {
+        company: "Hayward",
+        position: "Front-End Web Developer & Designer",
+        year: "2018 - 2022",
+        content: "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces."
+    },
+    {
+        company: "Turner Syndrome Fdn.",
+        position: "Lead Web Developer",
+        year: "2017 - 2018",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. "
+    }
+]
