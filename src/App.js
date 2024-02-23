bddfe299d81406162f5925df3e80304a8340cffa
Tan Bui <ttb39@scarletmail.rutgers.edu>
2024-02-22 23:20:11 -0800

    added bullet points and major css update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import { ResumeData } from './data';
 import { useState } from 'react';
 
 function ContentBody({data}){
-  console.log(data)
   return(
     <div className="content-body">
-      <h4>{data.position} @{data.company}</h4>
+      <h4>{data.position} @<a href={data.url} target="_blank" rel="noopener noreferrer">{data.company}</a></h4>
+      <p className="year">{data.year}</p>
       <p>{data.content}</p>
+      <ul className="content-list">{data.bulletPoints.map((item,i)=>(
+        <li key={`${i}-${item}`}>{item}</li>
+        ))}</ul>
+      <div className="skill-list">
+        {data.skillsArr.map((skill,i)=>{
+          return (
+            <div className="skill-item" key={`skill-${i}`}>{skill}</div>
+          )
+        })}
+      </div>
     </div>
   )
 }
@@ -24,22 +34,28 @@ function App() {
   return (
     <div className="resume-app container">
       <header className="app-header">
-        <h3>My Experience</h3>
+        <h3 className="mb-5 mt-1">My Experience</h3>
       </header>
       <div className="row">
         <div className="col-4">
-        <nav className='nav-list' role="tablist">
-          {ResumeData.map((item,i)=>(
-            <button
-              className={i == index ? 'active' : ''}
-              tabIndex={i} key={item.company}
-              onClick={handleClick}>{item.company}
-            </button>
-          ))}
-          </nav>
+          <nav className='nav-list' role="tablist">
+            {ResumeData.map((item,i)=>(
+              <button
+                className={'py-3' + (i == index ? ' active' : '')}
+                tabIndex={i} key={item.company}
+                onClick={handleClick}>{item.company}
+              </button>
+            ))}
+              <a
+                className='cta'
+                href="/assets/documents/Tan-Bui-Web-Developer-Designer-Resume-2024.pdf" 
+                target='_blank'>View Full Résumé
+                </a>
+            </nav>
         </div>
-            <div className="col-8"> <ContentBody data={ResumeData[index]} /></div>
-       
+            <div className="col-8">
+              <ContentBody data={ResumeData[index]} />
+            </div>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,19 +1,49 @@
 export const ResumeData = [{
         company: "Freelance",
+        url: "#",
         position: "Web Consultant",
         year: "2018 - Present",
-        content: "Build, style, and ship high-quality websites, design systems, mobile apps, and digital experiences for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and spearheading the development of internal tools."
+        content: "Build, style, and ship high-quality websites, design systems, mobile apps, and digital experiences for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and spearheading the development of internal tools.",
+        bulletPoints: [
+            'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more',
+            'Work alongside creative directors to lead the research, development, and architecture of technical solutions to fulfill business requirements',
+            'Collaborate with designers, project managers, and other engineers to transform creative concepts into production realities for clients and stakeholders',
+            'Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship'
+        ],
+        skillsArr: [
+            'HTML', 'Javascript', 'React', 'REST API', 'jQuery', 'NodeJS'
+        ]
     },
     {
         company: "Hayward",
+        url: "#",
         position: "Front-End Web Developer & Designer",
         year: "2018 - 2022",
-        content: "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces."
+        content: "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.",
+        bulletPoints: [
+            'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more',
+            'Work alongside creative directors to lead the research, development, and architecture of technical solutions to fulfill business requirements',
+            'Collaborate with designers, project managers, and other engineers to transform creative concepts into production realities for clients and stakeholders',
+            'Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship'
+        ],
+        skillsArr: [
+            'HTML', 'SASS', 'Javascript', 'React', 'REST API', 'jQuery', 'Salesforce'
+        ]
     },
     {
         company: "Turner Syndrome Fdn.",
+        url: "#",
         position: "Lead Web Developer",
         year: "2017 - 2018",
-        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. "
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.",
+        bulletPoints: [
+            'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more',
+            'Work alongside creative directors to lead the research, development, and architecture of technical solutions to fulfill business requirements',
+            'Collaborate with designers, project managers, and other engineers to transform creative concepts into production realities for clients and stakeholders',
+            'Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship'
+        ],
+        skillsArr: [
+            'HTML', 'Javascript', 'CSS', 'PHP', 'WordPress', 'CSS'
+        ]
     }
 ]
