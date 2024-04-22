47b1ddb4bcf8ea921c9b1ae3c0a826f42b42ed26
Thiettan <ttb39@scarletmail.rutgers.edu>
2024-04-22 17:32:55 -0400

    Update resume links, icon + some  mobile styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
 import { ResumeData } from './data';
-import { useState, useTransition} from 'react';
+import { useState, useTransition } from 'react';
 
-function ContentBody({data}){
-  return(
+function ContentBody({ data }) {
+  return (
     <div className="content-body">
       <h4>{data.position} @<a href={data.url} target="_blank" rel="noopener noreferrer">{data.company}</a></h4>
       <p className="year">{data.year}</p>
       <p>{data.content}</p>
-      <ul className="content-list">{data.bulletPoints.map((item,i)=>(
+      <ul className="content-list">{data.bulletPoints.map((item, i) => (
         <li key={`${i}-${item}`}>{item}</li>
-        ))}</ul>
+      ))}</ul>
       <div className="skill-list">
-        {data.skillsArr.map((skill,i)=>{
+        {data.skillsArr.map((skill, i) => {
           return (
             <div className="skill-item" key={`skill-${i}`}>{skill}</div>
           )
         })}
       </div>
       <a
-                className='cta cta-mobile'
-                href="/assets/documents/Tan-Bui-Web-Developer-Designer-Resume-2024.pdf"
-                target='_blank' rel="noopener noreferrer">View Full Résumé
-                </a>
+        className='cta cta-mobile'
+        href="/assets/documents/Tan-Bui-Web-Developer-Designer-Resume-2024-v2.pdf"
+        target='_blank' rel="noopener noreferrer">View Résumé <svg class="cta-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#64FFDA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+      </a>
     </div>
   )
 }
@@ -30,10 +30,10 @@ function ContentBody({data}){
 function App() {
 
   const [isPending, startTransition] = useTransition()
-  const [index,setIndex] = useState(0)
+  const [index, setIndex] = useState(0)
 
-  function handleClick(e){
-    startTransition(()=>{
+  function handleClick(e) {
+    startTransition(() => {
       setIndex(parseInt(e.target.attributes.index.value))
     })
 
@@ -47,7 +47,7 @@ function App() {
       <div className="row">
         <div className="col-md-4">
           <nav className='nav-list' role="tablist">
-            {ResumeData.map((item,i)=>(
+            {ResumeData.map((item, i) => (
               <button
                 className={'py-3' + (i == index ? ' active' : '')}
                 role="tab"
@@ -58,16 +58,16 @@ function App() {
                 onClick={handleClick}>{item.company}
               </button>
             ))}
-              <a
-                className='cta'
-                href="/assets/documents/Tan-Bui-Web-Developer-Designer-Resume-2024.pdf"
-                target='_blank' rel="noopener noreferrer">View Full Résumé
-                </a>
+            <a
+              className='cta'
+              href="/assets/documents/Tan-Bui-Web-Developer-Designer-Resume-2024-v2.pdf"
+              target='_blank' rel="noopener noreferrer">View Résumé <svg class="cta-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#64FFDA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+            </a>
           </nav>
         </div>
-            <div className="col-md-8">
-              <ContentBody data={ResumeData[index]} />
-            </div>
+        <div className="col-md-8">
+          <ContentBody data={ResumeData[index]} />
+        </div>
       </div>
     </div>
   );
